b725e623379d45d1813eecd0fbef94d406e10065
Anton Shakov <annawinnern1@gmail.com>
2023-11-20 23:44:27 +0300

    exception-theory2-DONE

--- src/com/shakov/lesson25/exception/ExceptionExample.java
+++ src/com/shakov/lesson25/exception/ExceptionExample.java
@@ -9,9 +9,12 @@ public class ExceptionExample {
 
         System.out.println("main was started");
         try {
+            System.out.println(finallyTest());
             unsafe(10);
         } catch (FileNotFoundException | TimeoutException e) {
             e.printStackTrace();
+        } finally {
+            System.out.println("finally");
         }
         System.out.println("main was ended");
     }
@@ -20,6 +23,24 @@ public class ExceptionExample {
         System.out.println("unsafe was started");
         if (value > 0) {
             throw new FileNotFoundException();
+        }
+        System.out.println("unsafe was ended");
+    }
+
+    public static int finallyTest() {
+        try {
+            return 2;
+        } catch (Throwable throwable) {
+            return 3;
+        } finally {
+            return 4;
+        }
+    }
+
+    public static void unsafeWithRuntime(int value) {
+        System.out.println("unsafe was started");
+        if (value > 0) {
+            throw new RuntimeException();
         }
         System.out.println("unsafe was ended");
     }
