f63211e96ab3a59e3132c1d8e85775e2b798e299
Anton Shakov <annawinnern1@gmail.com>
2023-11-20 23:55:51 +0300

    exception-task2-DONE

---
+++ src/com/shakov/lesson25/exception/tasks/Task2.java
@@ -0,0 +1,22 @@
+package com.shakov.lesson25.exception.tasks;
+
+/**
+ * 2. Написать код, который создаст, а затем отловит
+ * ArrayIndexOutOfBoundsException.
+ */
+
+public class Task2 {
+
+    public static void main(String[] args) {
+
+        int[] values = {1,2,3,4,5};
+
+        try {
+            for (int i = 0; i <= values.length; i++) {
+                System.out.println(values[i]);
+            }
+        } catch (ArrayIndexOutOfBoundsException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
