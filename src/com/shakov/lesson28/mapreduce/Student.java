4322e4b41f31df444df88a1ef9b9e7bd80f00198
Anton Shakov <annawinnern1@gmail.com>
2023-11-22 21:56:06 +0300

    optional-DONE

--- src/com/shakov/lesson28/mapreduce/Student.java
+++ src/com/shakov/lesson28/mapreduce/Student.java
@@ -1,13 +1,29 @@
 package com.shakov.lesson28.mapreduce;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student {
 
     private int age;
     private String name;
+    private List<Integer> marks = new ArrayList<>();
 
     public Student(int age, String name) {
         this.age = age;
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public List<Integer> getMarks() {
+        return marks;
     }
 
     public int getAge() {
